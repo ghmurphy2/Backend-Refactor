c8f5019ca299b5094fbc2c9ee7463191ff2127b6
Gavin Murphy <gavinhmurphy@gmail.com>
2021-11-29 20:37:09 -0800

    added mutationjs client side

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,91 @@
+// login, add user, add book ,remove book
+
+import gql from "graphql-tag";
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!){
+        addUser(username: $username, email: $email, password $password){
+            token
+            user{
+                _id
+                username
+                bookCount
+                email
+                savedBooks{
+                    title
+                    bookId
+                    link
+                    description
+                    authors
+                    image
+                    
+                }
+
+            }
+
+
+        }
+    }`;
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+        user{
+            _id
+            username
+            bookCount
+            email
+            savedBooks{
+                title
+                bookId
+                link
+                description
+                authors
+                image
+                
+            }
+        }
+    }
+}`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+                _id
+                username
+                bookCount
+                email
+                savedBooks{
+                    title
+                    bookId
+                    link
+                    description
+                    authors
+                    image
+                    
+                }
+            }
+        }
+    }`;
+
+    export const SAVE_BOOK = gql`
+    mutation saveBok($input: bookInput!) {
+        saveBook(input: $input) {
+            _id
+            username
+            bookCount
+            email
+            savedBooks{
+                title
+                bookId
+                link
+                description
+                authors
+                image
+                
+            }
+        }
+    }
+}`;
+
+
