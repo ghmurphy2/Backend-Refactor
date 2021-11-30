b443e9ec4e97508ce4ab2aa18400f6d985e89da8
Gavin Murphy <gavinhmurphy@gmail.com>
2021-11-29 20:40:54 -0800

    added apollo to client server.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,24 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+// add apollo client and provider
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  request: operation => {
+    // check local for token
+    const token = localStorage.getItem('id_token');
+// set context and token require
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql'
+});
+
 
 function App() {
   return (
