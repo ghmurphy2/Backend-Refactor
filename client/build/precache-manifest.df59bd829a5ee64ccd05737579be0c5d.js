ca8ff38521f81ea4283492db51bf21ce949aea2c
Gavin Murphy <gavinhmurphy@gmail.com>
2021-11-30 18:59:54 -0800

    bug

---
+++ client/build/precache-manifest.df59bd829a5ee64ccd05737579be0c5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6ee03d997543bfa932b2705918d8c58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83ab0c78548c6770d5de",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "9bdb5eac7a8c283d67b3",
+    "url": "/static/css/main.10507661.chunk.css"
+  },
+  {
+    "revision": "83ab0c78548c6770d5de",
+    "url": "/static/js/2.ba4fa3b3.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.ba4fa3b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bdb5eac7a8c283d67b3",
+    "url": "/static/js/main.416f0bf1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
