931c5a6a1d3ccf8207ec304535d3df2357ce21e0
Gavin Murphy <gavinhmurphy@gmail.com>
2021-11-30 17:15:19 -0800

    heroku bug

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+const routes = require("./routes");
 // added imports
-const { typeDefs, resolvers } = require('./schemas')
-const { authMiddleware } = require('./utils/auth')
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 const { ApolloServer } = require("apollo-server-express");
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,20 +14,21 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context:authMiddleware
+  context: authMiddleware,
 });
 
-server.applyMiddleware({ app })
+await server.start();
+server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.use(routes);
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
