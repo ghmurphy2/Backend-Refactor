dbc1b22d7fc0d4e47f298d2c6d3688b605f08e61
Gavin Murphy <gavinhmurphy@gmail.com>
2021-11-30 17:18:00 -0800

    heroku deploy issue

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-await server.start();
+// await server.start();
 server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -27,7 +27,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
